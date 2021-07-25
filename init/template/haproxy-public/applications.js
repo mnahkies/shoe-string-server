f09762ee0013721efb34e019277ad1d9d97812d8
Michael Nahkies <michael@nahkies.co.nz>
2021-07-25 14:59:27 +0100

    delete unused files from template

--- init/template/haproxy-public/applications.js
+++
@@ -1,7 +0,0 @@
-module.exports.applications = [
-    {
-        containerName: "example-application-com",
-        containerPort: 80,
-        hostName: ["www.example-application-com", "example-application-com"]
-    }
-]
