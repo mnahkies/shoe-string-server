9476ce7a2921d8ccd417dc39f9ccec3a95780d74
Michael Nahkies <michael@nahkies.co.nz>
2024-09-22 15:06:50 +0100

    fix: compatible with latest docker-compose (#9)

--- applications/watchdog-up.sh
+++ applications/watchdog-up.sh
@@ -8,13 +8,17 @@ source "${__dir}"/../config.sh
 INTERNAL_APPLICATIONS="${DATA_BASE_PATH}/applications-internal/*.yaml"
 
 for APP in $INTERNAL_APPLICATIONS; do
-  docker-compose -f "$APP" -p "$APP" up -d
+  echo $APP
+  APP_NAME=$(basename $APP ".yaml")
+  docker-compose --file "$APP" -p $APP_NAME up -d --force-recreate --remove-orphans
 done
 
 PUBLIC_APPLICATIONS="${DATA_BASE_PATH}/applications-public/*.yaml"
 
 for APP in $PUBLIC_APPLICATIONS; do
-  docker-compose -f "$APP" -p "$APP" up -d
+  echo $APP
+  APP_NAME=$(basename $APP ".yaml")
+  docker-compose --file "$APP" -p $APP_NAME up -d --force-recreate --remove-orphans
 done
 
 "$__dir"/../proxy/reload-haproxy-config.sh
