d3ff3c57ee4eaa2d916a6ae0c5f965150ed1482d
Michael Nahkies <michael@nahkies.co.nz>
2025-09-14 12:11:24 +0100

    fix: watchdog-down when no public applications

--- init/template/config.sh
+++ init/template/config.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
+source "${DATA_BASE_PATH}/config.secrets.sh"
+
 export LETS_ENCRYPT_EMAIL_ADDRESS=support@example.com
-
-# if using ssl certs for proxy_internal *and* using zonomi as a dns provider
-export ZONOMI_API_KEY=""
-
-export POSTGRES_PASSWORD="<password>"
 
 # On AWS you can find these addresses by:
 # curl http://169.254.169.254/latest/meta-data/public-ipv4
 # curl http://169.254.169.254/latest/meta-data/local-ipv4
-
 export PUBLIC_IP=127.0.0.1
 export PUBLIC_PORT_HTTP=80
 export PUBLIC_PORT_HTTPS=443
@@ -19,7 +15,7 @@ export PUBLIC_PORT_HTTPS=443
 # $(tailscale ip -4)
 export PRIVATE_IP=10.12.0.1
 export PRIVATE_PORT=80
-export PUBLIC_PORT_HTTPS=443
+export PRIVATE_PORT_HTTPS=443
 
 # Space separated list of additional docker bridge networks to create
 export ADDITIONAL_NETWORKS=()
