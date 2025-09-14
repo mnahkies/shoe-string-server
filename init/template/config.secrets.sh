ecfc81cedfb20c7b51415d6215e212af0cea9f0c
Michael Nahkies <michael@nahkies.co.nz>
2025-09-14 12:12:07 +0100

    fix: watchdog-down when no public applications (#18)

---
+++ init/template/config.secrets.sh
@@ -0,0 +1,8 @@
+# if using ssl certs for proxy_internal *and* using zonomi as a dns provider
+export ZONOMI_API_KEY=""
+
+# default postgres user password
+export POSTGRES_PASSWORD="<password>"
+
+export POSTGRES_EXPORTER_POSTGRES_USER="postgres_exporter"
+export POSTGRES_EXPORTER_POSTGRES_PASSWORD=""
